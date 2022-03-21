ba0f22293826fc5128456632603f0b992af4c2c6
Mike12351 <hohmhmhm@gmail.com>
2022-03-21 16:59:55 +0100

    Improved spawn function
    When leaving and entering a new chunk, spawn new chunks at the edge of the maze to give a feel of an infinite maze.

--- Unity Project/Assets/Scripts/ChunkManager.cs
+++ Unity Project/Assets/Scripts/ChunkManager.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class ChunkManager : MonoBehaviour
 {
+    //chunkprefabs array stores all the possible prefabs that can be instantiated
     public GameObject[] chunkPrefabs;
 
+    //playerTransform is the player controller
     private Transform playerTransform;
-    private float chunkLength = 20f;
+
+    //reusable variables for ease of computations
+    private int chunkLength = 20;
     private int arrayLength;
+
+    // previous and new players coordinates, used to find out whether a player has left and entered a new chunk
+    private int previous_playerX = 0; 
+    private int previous_playerZ = 0;
+    private int new_playerX = 0;
+    private int new_playerZ = 0;
+
+    //Dictionary to stores data about the current maze
+    //Keys are the coordinates on where the chunk is placed on the xz axis
+    //value is of type Vector3?4 to store exact coordinates of the chunk and rotation and type of chunk
+    private Dictionary<Vector2, Vector3> loadedChunks = new Dictionary<Vector2, Vector3>();
 
     // Start is called before the first frame update
     void Start()
@@ -23,9 +38,37 @@ public class ChunkManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //check if player has left/entered a new chunk
+        if (playerTransform.position.x < 0)
+        {
+            new_playerX = (int)(playerTransform.position.x - chunkLength) / chunkLength;
+        }
+        else
+        {
+            new_playerX = (int)(playerTransform.position.x) / chunkLength;
+        }
+        if (playerTransform.position.z > 0)
+        {
+            new_playerZ = (int)(playerTransform.position.z + chunkLength) / chunkLength;
+        }
+        else
+        {
+            new_playerZ = (int)(playerTransform.position.z) / chunkLength;
+        }
+
+        //if entered a new chunk, check which axis and which direction and call the spawn new chunk functions
+        if (previous_playerX != new_playerX)
+        {
+            movedChunkX(previous_playerX, new_playerX);
+            previous_playerX = new_playerX;
+        }else if (previous_playerZ != new_playerZ)
+        {
+            movedChunkZ(previous_playerZ, new_playerZ);
+            previous_playerZ = new_playerZ;
+        }
     }
 
+    //spawns a new chunk at the given location, with random orientation and random type
     private void spawnChunk(float x, float z, int prefabIndex = -1)
     {
         GameObject go;
@@ -50,8 +93,11 @@ public class ChunkManager : MonoBehaviour
         go.transform.position = Vector3.right * x * chunkLength + Vector3.forward * z * chunkLength;
         go.transform.position += new Vector3(rotVec.x, 0f, rotVec.y);
 
+        loadedChunks.Add(new Vector2(x, z), new Vector3(go.transform.position.x,go.transform.position.z,go.transform.rotation.y));
+
     }
 
+    //inital fucntion to set up the base maze 5x5
     private void initChunks()
     {
 
@@ -64,6 +110,7 @@ public class ChunkManager : MonoBehaviour
         }
     }
 
+    //function used to help with the orientation of the chunks
     private Vector2 rotateChunk(int index)
     {
         if (index == 0)
@@ -79,6 +126,56 @@ public class ChunkManager : MonoBehaviour
         else
         {
             return new Vector2(0, -chunkLength);
+        }
+    }
+
+    //when moving in the x axis create new chunks at the edge if not already created
+    private void movedChunkX(int previousX, int newX)
+    {
+        int spawnX;
+        int spawnZ;
+
+        if (newX - previousX ==1)
+        {
+            spawnX = newX + 2;
+        }
+        else
+        {
+            spawnX = newX - 2;
+        }
+
+        for (int i = -2; i<=2; i++)
+        {
+            spawnZ = new_playerZ + i;
+            if (!loadedChunks.ContainsKey(new Vector2(spawnX, spawnZ)))
+            {
+                spawnChunk(spawnX, spawnZ);
+            }
+        }
+    }
+
+    //when moving in the z axis create new chunks at the edge if not already created
+    private void movedChunkZ(int previousZ, int newZ)
+    {
+        int spawnX;
+        int spawnZ;
+
+        if (newZ - previousZ == 1)
+        {
+            spawnZ = newZ + 2;
+        }
+        else
+        {
+            spawnZ = newZ - 2;
+        }
+
+        for (int i = -2; i <= 2; i++)
+        {
+            spawnX = new_playerX + i;
+            if (!loadedChunks.ContainsKey(new Vector2(spawnX, spawnZ)))
+            {
+                spawnChunk(spawnX, spawnZ);
+            }
         }
     }
 }
