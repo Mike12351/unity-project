c0d457df8ee705d5ce3f316c3691872e24a9fcc0
Mike12351 <93015418+Mike12351@users.noreply.github.com>
2022-05-04 17:22:39 +0200

    commented enemy move script

--- Unity Project/Assets/Scripts/EnemyAgentMove.cs
+++ Unity Project/Assets/Scripts/EnemyAgentMove.cs
@@ -5,23 +5,33 @@ using UnityEngine.AI;
 
 public class EnemyAgentMove : MonoBehaviour
 {
+    //the navmesh agent
     public NavMeshAgent agent;
 
+    //array of all the point this enemy can randomly go to
     public Transform[] patrolPoints;
 
+    //first index is chosen randomly
+    //selected index is used to check whether we chose the point that the enemy is currently on
     private int index;
     private int currentIndex = 0;
 
+    //reached to check whether we have reached the goal
     private bool reached = true;
+    //coordinates of the point to go to
     private Vector3 nextPoint;
 
+    //flag to check whether the agent has been enabled
     private bool flag = false;
 
+    //the constant used to determine whether we have arrived at the goal
     private const float threshold = 0.2f;
 
     // Start is called before the first frame update
     void Start()
     {
+        //start by setting the enemy to the start position
+        //and enable the agent component and tus the flag as well
         transform.position = patrolPoints[0].position;
         agent = GetComponent<NavMeshAgent>();
         agent.enabled = true;
@@ -35,6 +45,7 @@ public class EnemyAgentMove : MonoBehaviour
         //otherwise repeat and set a new goal point
         if (reached)
         {
+            //choose index randomly and then compute the destination
             index = Random.Range(0, patrolPoints.Length);
             nextPoint = patrolPoints[index].position;
             nextPoint = new Vector3(nextPoint.x, nextPoint.y, nextPoint.z);
@@ -49,6 +60,7 @@ public class EnemyAgentMove : MonoBehaviour
             }
         }
 
+        //pathpending to check whether the agent is done with calculating the path
         if (!agent.pathPending && !reached && flag)
         {
             if (agent.remainingDistance < threshold)
