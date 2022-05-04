5435449e2b559c700241d4613958b0a19239ff96
Mike12351 <hohmhmhm@gmail.com>
2022-05-04 16:56:45 +0200

    fixed enemy spawn

--- Unity Project/Assets/Scripts/EnemyAgentMove.cs
+++ Unity Project/Assets/Scripts/EnemyAgentMove.cs
@@ -15,12 +15,16 @@ public class EnemyAgentMove : MonoBehaviour
     private bool reached = true;
     private Vector3 nextPoint;
 
+    private bool flag = false;
+
     private const float threshold = 0.2f;
 
     // Start is called before the first frame update
     void Start()
     {
         transform.position = patrolPoints[0].position;
+        agent = GetComponent<NavMeshAgent>();
+        agent.enabled = true;
     }
 
     // Update is called once per frame
@@ -32,20 +36,28 @@ public class EnemyAgentMove : MonoBehaviour
         {
             index = Random.Range(0, patrolPoints.Length);
             nextPoint = patrolPoints[index].position;
-            reached = false;
             nextPoint = new Vector3(nextPoint.x, nextPoint.y, nextPoint.z);
-            //if (currentIndex == index)
-            agent.SetDestination(nextPoint);
+            if (currentIndex == index)
+            {
+                reached = true;
+            }
+            else
+            {
+                reached = false;
+                agent.SetDestination(nextPoint);
+            }
+        }
+
+        if (!agent.pathPending && !reached && flag)
+        {
+            if (agent.remainingDistance < threshold)
+            {
+                print("arrived");
+                reached = true;
+                Destroy(this);
+            }
         }
         // stay in chunk and just move around
-
-        if (agent.remainingDistance < threshold && !reached)
-        {
-            //if the remaining distance is less than the threshold then we assume that the player has arrived
-            //at the destination
-            reached = true;
-            print("reached destination");
-        }
             // if distance from enemy to player has reached a certain value (can be changed later via dynamic changes)
             //then target player
             //otherwise go back to initial pos
